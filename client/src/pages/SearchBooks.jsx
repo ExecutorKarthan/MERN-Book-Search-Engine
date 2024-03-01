5c98008830a9353348fe95e1ddae391f43d0b576
Alex Messina <me@alexmessina.dev>
2024-03-01 10:38:59 -0600

    Added hook and functions to save a searched book

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -9,10 +9,14 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
+import { SAVE_BOOK } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+
 const SearchBooks = () => {
+  const [saveBook, {error}] = useMutation(SAVE_BOOK)
+
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
@@ -72,7 +76,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const {data} = await saveBook({
+        variables: {...bookToSave}
+      })
 
       if (!response.ok) {
         throw new Error('something went wrong!');
