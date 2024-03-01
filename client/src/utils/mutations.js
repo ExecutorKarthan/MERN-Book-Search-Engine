ea1e82350d9bfef59b4b08aa16c9e79a12486cd6
Alex Messina <me@alexmessina.dev>
2024-03-01 10:17:16 -0600

    Finished adding code for database mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -8,3 +8,42 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: STRING!) {
+    addUser(username: $username, email: $email, password: $password) {
+      username
+      email
+      password
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($book: BookInput!) {
+    saveBook(bookInput: $book) {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+`;
+
+
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($book: String!) {
+    removeBook(book: $book) {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+      books
+    }
+  }
+`;
