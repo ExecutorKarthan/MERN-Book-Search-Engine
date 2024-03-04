97e6655a50280308a73fbd549133d691ff09c198
Alex Messina <me@alexmessina.dev>
2024-03-04 13:10:13 -0600

    Update server.js
    Removed production in an attempt to remove module error

--- server/server.js
+++ server/server.js
@@ -24,15 +24,6 @@ const startApolloServer = async () => {
     context: authMiddleware
   }));
 
-  // if we're in production, serve client/build as static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
-
   db.once('open', () => {
     app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
   });
